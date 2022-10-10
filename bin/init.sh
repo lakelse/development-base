774ff7d29b5353515eb8ec63380fa153714bfe71
lakelse <jonathan.macdonald@gmail.com>
2022-10-09 21:23:15 -0500

    Set ansible_host based on host OS
    127.0.0.1 if on Linux, host.docker.internal if not.

--- bin/init.sh
+++ bin/init.sh
@@ -4,19 +4,35 @@ export CONTAINER_NAME="ansible"
 
 PATH_TO_SSH_KEY="$PWD/keys/id_rsa"
 
-case "$( uname -s )" in
+UNAME_OUTPUT=$( uname -s )
+
+case "$UNAME_OUTPUT" in
+  Linux*)
+    HOST_OS=Linux
+    ;;
+  Darwin*)
+    HOST_OS=Mac
+    ;;
   CYGWIN*)
+    HOST_OS=Windows
     PWD=$( cygpath --dos $PWD )
     PATH_TO_SSH_KEY=$( cygpath --dos $PATH_TO_SSH_KEY )
     ;;
+  MINGW*)
+    HOST_OS=Windows
+    ;;
+  *) HOST_OS="UNKNOWN:${UNAME_OUTPUT}"
 esac
+
+export HOST_OS
 
 if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME )" != "true" ]
 then 
 
   docker run \
-    -d \
+    --detach \
     --name $CONTAINER_NAME \
+    --env HOST_OS=$HOST_OS \
     --network=host \
     --rm \
     -it \
